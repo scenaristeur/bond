18ae18818d63a83cacec788b418a866e0afdb1ea
David <scenaristeur@gmail.com>
2020-11-20 12:37:15 +0100

    todo

---
+++ todo/README.md
@@ -0,0 +1,1 @@
+https://codesandbox.io/s/vue-todo-list-app-with-single-file-component-vzkl3?file=/webpack.config.js:0-1814
---
+++ todo/src/TodoItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <span>{{ todo.text }}</span>
+    <button @click.prevent="$emit('remove', todo)">Remove</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+li
+  display flex
+  margin 5px 0
+
+  span
+    flex 1
+
+  button
+    border 1px solid orange
+    background orange
+    color white
+    font-size 0.8rem
+    padding 2px 4px
+    cursor pointer
+
+    &:hover
+      border-color #ff8100
+      background #ff8100
+</style>
